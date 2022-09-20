6ee1eed46fbe2861ec6d43a33933af9f3fe05815
danielafriyie <afriyiedaniel1@outlook.com>
2022-09-20 22:06:36 +0000

    added type annotations to selenium_utils package

--- ru/selenium_utils/__init__.py
+++ ru/selenium_utils/__init__.py
@@ -13,12 +13,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import random as rd
 import time
+import typing
 import warnings
 import sys
+import random as rd
 
 try:
+    from selenium.webdriver.remote.webdriver import WebDriver, WebElement
     from selenium.webdriver.support import expected_conditions as ec
     from selenium.webdriver.common.action_chains import ActionChains
     from selenium.webdriver.support.wait import WebDriverWait
@@ -37,28 +39,38 @@ except ImportError:
     warnings.warn("It seems you don't have pyperclip installed. "
                   "Install it before using this module!\npip3 install pyperclip")
 
+Element = typing.Union[WebElement, typing.List[WebElement]]
+Condition = typing.Optional[typing.Callable[[typing.Tuple[str, str]], typing.Any]]
+Seconds = typing.Optional[typing.Union[int, float]]
+Args = typing.Tuple[typing.Any, ...]
+Kwargs = typing.Dict[str, typing.Any]
+Action = typing.Optional[str]
 
-def window_scroll_to(driver, loc):
+
+def window_scroll_to(driver: WebDriver, loc: typing.Union[int, float]) -> None:
     driver.execute_script(f"window.scrollTo(0, {loc});")
 
 
-def scroll_into_view(driver, element, offset=200):
+def scroll_into_view(driver: WebDriver, element: WebElement, offset: typing.Optional[int] = 200) -> None:
     window_scroll_to(driver, element.location['y'] - offset)
 
 
-def scroll_into_view_js(driver, element):
+def scroll_into_view_js(driver: WebDriver, element: WebElement) -> None:
     driver.execute_script("arguments[0].scrollIntoView();", element)
 
 
-def _driver_wait(driver,
-                 locator,
-                 by,
-                 secs=10,
-                 condition=ec.element_to_be_clickable,
-                 action=None,
-                 *args, **kwargs):
-    wait = WebDriverWait(driver=driver, timeout=secs)
-    element = wait.until(condition((by, locator)))
+def _driver_wait(
+        driver: WebDriver,
+        locator: str,
+        by: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable,
+        action: Action = None,
+        *args: Args,
+        **kwargs: Kwargs
+) -> Element:
+    wait: WebDriverWait = WebDriverWait(driver=driver, timeout=secs)
+    element: Element = wait.until(condition((by, locator)))
     if action:
         if hasattr(element, action):
             action_func = getattr(element, action)
@@ -66,45 +78,84 @@ def _driver_wait(driver,
     return element
 
 
-def find_element_by_xpath(driver, xpath, secs=10, condition=ec.element_to_be_clickable, action=None, *args, **kwargs):
+def find_element_by_xpath(
+        driver: WebDriver,
+        xpath: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable,
+        action: Action = None,
+        *args: Args,
+        **kwargs: Kwargs
+) -> Element:
     return _driver_wait(driver, xpath, By.XPATH, secs, condition, action, *args, **kwargs)
 
 
-def find_element_by_css(driver, selector, secs=10, condition=ec.element_to_be_clickable, action=None, *args, **kwargs):
+def find_element_by_css(
+        driver: WebDriver,
+        selector: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable,
+        action: Action = None,
+        *args: Args,
+        **kwargs: Kwargs
+) -> Element:
     return _driver_wait(driver, selector, By.CSS_SELECTOR, secs, condition, action, *args, **kwargs)
 
 
-def find_element_by_id(driver, id, secs=10, condition=ec.element_to_be_clickable, action=None, *args, **kwargs):
+def find_element_by_id(
+        driver: WebDriver,
+        id: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable,
+        action: Action = None,
+        *args: Args,
+        **kwargs: Kwargs
+) -> Element:
     return _driver_wait(driver, id, By.ID, secs, condition, action, *args, **kwargs)
 
 
-def find_element_by_link_text(driver,
-                              text,
-                              secs=10,
-                              condition=ec.element_to_be_clickable,
-                              action=None,
-                              *args,
-                              **kwargs):
+def find_element_by_link_text(
+        driver: WebDriver,
+        text: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable,
+        action: Action = None,
+        *args: Args,
+        **kwargs: Kwargs
+) -> Element:
     return _driver_wait(driver, text, By.LINK_TEXT, secs, condition, action, *args, **kwargs)
 
 
-def driver_or_js_click(driver, xpath, secs=5, condition=ec.element_to_be_clickable):
+def driver_or_js_click(
+        driver: WebDriver,
+        xpath: str,
+        secs: Seconds = 5,
+        condition: Condition = ec.element_to_be_clickable
+) -> None:
     try:
-        elm = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
+        elm: WebElement = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
         ActionChains(driver).move_to_element(elm).click().perform()
     except WebDriverException:
-        elm = driver.find_element(By.XPATH, xpath)
+        elm: WebElement = driver.find_element(By.XPATH, xpath)
         try:
             ActionChains(driver).move_to_element(elm).click().perform()
         except WebDriverException:
             driver.execute_script("arguments[0].click()", elm)
 
 
-def manual_entry(driver, xpath, text, secs=10, condition=ec.element_to_be_clickable, sleep_time=0.05, *args, **kwargs):
-    if not isinstance(sleep_time, int) and not isinstance(sleep_time, float):
+def manual_entry(
+        driver: WebDriver,
+        xpath: str,
+        text: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable,
+        sleep_time: Seconds = 0.05,
+        *args: Args
+) -> None:
+    if (not isinstance(sleep_time, int)) and (not isinstance(sleep_time, float)):
         args += (sleep_time,)
         sleep_time = 0.05
-    elm = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
+    elm: WebElement = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
     ActionChains(driver).move_to_element(elm).perform()
     elm.clear()
     text = f"{text}"
@@ -112,37 +163,50 @@ def manual_entry(driver, xpath, text, secs=10, condition=ec.element_to_be_clicka
         elm.send_keys(letter)
         time.sleep(sleep_time)
     time.sleep(sleep_time)
-    elm.send_keys(*args, **kwargs)
+    elm.send_keys(*args)
 
 
-def enter(driver, xpath, text, secs=10, condition=ec.element_to_be_clickable, *args, **kwargs):
-    elm = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
+def enter(
+        driver: WebDriver,
+        xpath: str,
+        text: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable,
+        *args: Args
+) -> None:
+    elm: WebElement = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
     ActionChains(driver).move_to_element(elm).perform()
     elm.clear()
     text = f"{text}"
-    elm.send_keys(text, *args, **kwargs)
+    elm.send_keys(text, *args)
 
 
-def paste(driver, xpath, text, secs=10, condition=ec.element_to_be_clickable):
-    elm = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
+def paste(
+        driver: WebDriver,
+        xpath: str,
+        text: str,
+        secs: Seconds = 10,
+        condition: Condition = ec.element_to_be_clickable
+) -> None:
+    elm: WebElement = find_element_by_xpath(driver, xpath, secs=secs, condition=condition)
     pyperclip.copy(text)
     if sys.platform == 'darwin':
         ctrl = Keys.COMMAND
     else:
         ctrl = Keys.CONTROL
-    actions = ActionChains(driver)
+    actions: ActionChains = ActionChains(driver)
     actions.move_to_element(elm)
     actions.click()
     actions.key_down(ctrl)
-    actions.send_keys('V')
+    actions.send_keys('v')
     actions.key_up(ctrl)
     actions.perform()
 
 
-def random_delay(a=1, b=3):
-    delay = rd.randint(a, b)
-    precision = delay / (a + b)
-    sleep_time = delay + precision
+def random_delay(a: typing.Optional[int] = 1, b: typing.Optional[int] = 3) -> None:
+    delay: int = rd.randint(a, b)
+    precision: float = delay / (a + b)
+    sleep_time: float = delay + precision
     time.sleep(sleep_time)
 
 
