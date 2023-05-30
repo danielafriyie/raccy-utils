663812d474b41f8c03778951eb841d02103e8312
danielafriyie <afriyiedaniel1@outlook.com>
2023-05-30 11:57:20 +0000

    did some refactoring

--- java/ru/lib/src/main/java/raccy/seleniumutils/SeleniumUtils.java
+++ java/ru/lib/src/main/java/raccy/seleniumutils/SeleniumUtils.java
@@ -5,7 +5,11 @@ import java.lang.Thread;
 import java.time.Duration;
 import java.util.function.Function;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedCondition;
