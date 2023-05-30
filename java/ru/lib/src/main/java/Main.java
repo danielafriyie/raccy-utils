8bcdeacf4543b924d9aac527f3f713cbea053e5e
danielafriyie <afriyiedaniel1@outlook.com>
2023-05-30 10:34:22 +0000

    working on SeleniumUtils class

--- java/ru/lib/src/main/java/Main.java
+++ java/ru/lib/src/main/java/Main.java
@@ -1,9 +1,15 @@
-import raccy.config.TextConfig;
-import raccy.utils.Utils;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
+
+    static void go(List<? extends Integer> lst) {
+        System.out.println(lst);
+    }
+
     public static void main(String[] args) throws Exception {
-        String s = String.format("window.scrollTo(0, %s);",23.5);
-        System.out.println(s);
+        go(new ArrayList<>());
+        go(Arrays.asList(Integer.valueOf(23), Integer.valueOf(23), Integer.valueOf(2)));
     }
 }
--- java/ru/lib/src/main/java/raccy/seleniumutils/SeleniumUtils.java
+++ java/ru/lib/src/main/java/raccy/seleniumutils/SeleniumUtils.java
@@ -1,5 +1,9 @@
 package raccy.seleniumutils;
 
+import java.util.List;
+import java.time.Duration;
+import java.util.function.Function;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,8 +12,12 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SeleniumUtils {
+
+    private SeleniumUtils() {
+    }
 
     public static void windowScrollTo(WebDriver driver, Number location) {
         JavascriptExecutor executor = (JavascriptExecutor) driver;
@@ -21,5 +29,8 @@ public class SeleniumUtils {
         executor.executeScript("arguments[0].scrollIntoView();", element);
     }
 
-//    private static WebElement driverWait(WebDriver driver, String locator, By by, int timeout, ExpectedCondition condition)
+    public static WebElement driverWait(WebDriver driver, String locator, By by, int timeout, Function<By, ExpectedCondition<WebElement>> condition) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        return wait.until(condition.apply(by));
+    }
 }
