772e9e5838a2eedcc5cbc63e80604bdce1ac6edc
danielafriyie <afriyiedaniel1@outlook.com>
2023-05-31 15:26:49 +0000

    working on SLF4JLogger class

---
+++ java/ru/lib/src/main/java/raccy/logger/slf4j/SLF4JLogger.java
@@ -0,0 +1,28 @@
+package raccy.logger.slf4j;
+
+public class SLF4JLogger implements raccy.logger.Logger{
+    @Override
+    public void info(String msg) {
+
+    }
+
+    @Override
+    public void warning(String msg) {
+
+    }
+
+    @Override
+    public void success(String msg) {
+
+    }
+
+    @Override
+    public void error(String msg) {
+
+    }
+
+    @Override
+    public void error(Exception e) {
+
+    }
+}
