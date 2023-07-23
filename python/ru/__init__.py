e1880b78323272c178137b8dc29dfdfe55391a5b
danielafriyie <afriyiedaniel1@outlook.com>
2023-07-23 21:39:18 +0000

    bumped up version

--- python/ru/__init__.py
+++ python/ru/__init__.py
@@ -16,7 +16,7 @@ limitations under the License.
 from .config import JsonConfig, TextConfig
 from .logger import logger, ColorPrint
 
-__version__ = '0.3.12'
+__version__ = '0.3.13'
 
 __all__ = ['JsonConfig',
            'TextConfig',
