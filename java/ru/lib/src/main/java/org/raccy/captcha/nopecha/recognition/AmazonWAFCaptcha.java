d482ce0b0ade2968f51b2b912426e617959eb235
danielafriyie <afriyiedaniel1@outlook.com>
2023-07-25 19:07:06 +0000

    implemented AmazonWAFCaptcha

--- java/ru/lib/src/main/java/org/raccy/captcha/nopecha/recognition/AmazonWAFCaptcha.java
+++ java/ru/lib/src/main/java/org/raccy/captcha/nopecha/recognition/AmazonWAFCaptcha.java
@@ -1,9 +1,6 @@
 package org.raccy.captcha.nopecha.recognition;
 
 import java.util.Map;
-import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.IOException;
 
@@ -17,5 +14,46 @@ import org.raccy.captcha.RestAdapter;
 import org.raccy.captcha.nopecha.Nopecha;
 
 
-public class AmazonWAFCaptcha {
+public class AmazonWAFCaptcha extends Nopecha {
+    private String audio = null;  // Base64 encoded audio
+
+    public AmazonWAFCaptcha(String apiKey, Logger logger, int retries, int retryWaitTime) {
+        super(apiKey, "awscaptcha", logger, retries, retryWaitTime);
+    }
+
+    public AmazonWAFCaptcha(String apiKey, Logger logger) {
+        this(apiKey, logger, 6, 20);
+    }
+
+    public AmazonWAFCaptcha(String apiKey) {
+        this(apiKey, null);
+    }
+
+    public void setAudio(String audio) throws IOException {
+        this.audio = fileToBase64(audio);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> submitRequest() throws IOException {
+        if (audio == null)
+            throw new IllegalStateException("Audio not set!");
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("key", apiKey);
+        payload.put("type", type);
+        payload.put("audio_data", new String[]{audio});
+
+        String payloadStr = gson.toJson(payload);
+
+        RequestBody body = RequestBody.create(payloadStr, JSON);
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .post(body)
+                .build();
+
+        try (Response response = adapter.request(Method.POST, BASE_URL, request);) {
+            return RestAdapter.toJSON(response, Map.class);
+        }
+    }
 }
