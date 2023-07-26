7f1651d5b3aba7bb246626d3d67fa98879603d80
danielafriyie <afriyiedaniel1@outlook.com>
2023-07-26 10:02:37 +0000

    added ArkoseFunCaptcha

---
+++ java/ru/lib/src/main/java/org/raccy/captcha/nopecha/recognition/ArkoseFunCaptcha.java
@@ -0,0 +1,64 @@
+package org.raccy.captcha.nopecha.recognition;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.RequestBody;
+import org.apache.logging.log4j.Logger;
+
+import org.raccy.captcha.Method;
+import org.raccy.captcha.RestAdapter;
+import org.raccy.captcha.nopecha.Nopecha;
+
+public class ArkoseFunCaptcha extends Nopecha {
+    private String image = null;  // Base64 encoded image
+    private String task = null;
+
+    public ArkoseFunCaptcha(String apiKey, Logger logger, int retries, int retryWaitTime) {
+        super(apiKey, "funcaptcha", logger, retries, retryWaitTime);
+    }
+
+    public ArkoseFunCaptcha(String apiKey, Logger logger) {
+        this(apiKey, logger, 6, 20);
+    }
+
+    public ArkoseFunCaptcha(String apiKey) {
+        this(apiKey, null);
+    }
+
+    public void setImage(String image) throws IOException {
+        this.image = fileToBase64(image);
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> submitRequest() throws IOException {
+        if ((image == null) || (task == null))
+            throw new IllegalStateException("Image or task is not set!");
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("key", apiKey);
+        payload.put("type", type);
+        payload.put("task", task);
+        payload.put("image_data", new String[]{image});
+
+        String payloadStr = gson.toJson(payload);
+
+        RequestBody body = RequestBody.create(payloadStr, JSON);
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .post(body)
+                .build();
+
+        try (Response response = adapter.request(Method.POST, BASE_URL, request);) {
+            return RestAdapter.toJSON(response, Map.class);
+        }
+    }
+}
